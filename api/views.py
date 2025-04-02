81a49d54590a9d4b78623f2605ed69f695a8648a
juned9296 <junedahmad9296@gmail.com>
2025-04-02 12:41:13 +0530

    views file

--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response, status
+from rest_framework import generics, permissions, status
 # Create your views here.
