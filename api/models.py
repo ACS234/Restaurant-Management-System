0890ef6d037d00fa1402aaebe8b7be6f216c8fe8
Priyesh <priyeshm845@gmail.com>
2025-04-03 10:28:59 +0530

    i remove some space

--- api/models.py
+++ api/models.py
@@ -16,8 +16,8 @@ class Restaurant(models.Model):
     def __str__(self):
         return self.name
     
-    
-    
+
+ 
 class Reservation(models.Model):
     restaurant = models.ForeignKey(Restaurant, related_name='reservations', on_delete=models.CASCADE)
     customer_name = models.CharField(User,max_length=255)
