43be4bd89db454edd55f0ba66caa2341de703a29
ACS234 <naurangilal@acstechconsulting.com>
2025-04-03 11:02:48 +0530

    JWT Integration complete

--- restaurent_management_system/urls.py
+++ restaurent_management_system/urls.py
@@ -21,6 +21,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('auth/', include('authapp.urls')),
     path('api/', include('api.urls')),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
